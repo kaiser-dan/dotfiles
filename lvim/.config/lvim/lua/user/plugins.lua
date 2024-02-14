e65516bcb18869e16e5b71217cf6a558e83e3e27
Daniel Kaiser <dkaiser4897@gmail.com>
2024-02-13 23:42:28 -0500

    2024-02-13v2

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -20,6 +20,24 @@ lvim.plugins = {
     "mfussenegger/nvim-dap-python",
     "nvim-neotest/neotest-python",
     -- Navigation
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            -- { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
     "kevinhwang91/rnvimr",
     {
         "ThePrimeagen/harpoon",
